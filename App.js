d61f3d8658cd38b7fe2547d9db4f8e76ece9a742
puemmth <puem.mth@gmail.com>
2023-07-19 15:38:18 +0700

    19/7/23

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import FlatListScore from './components/FlatListScore';
 import SectionListScore from './components/SectionListScore.js';
 
-export default function App() {
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AnotherScreen = ({ navigation }) => {
   return (
-    // <FlatListScore />
-    <SectionListScore />
+    <View>
+      <Text>Another Screen</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("HomeScreen")
+        }}
+        title='Navigate to HomeScreen'
+      />
+
+    </View>
+  )
+}
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("AnotherScreen")
+        }}
+        title='Check to Another'
+      />
+    </View>
+  )
+}
+
+export default function App() {
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='HomeScreen'>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="AnotherScreen" component={AnotherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
